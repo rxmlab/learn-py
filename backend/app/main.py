3ecc9ad2aa2993d671118f5d9606cc56eb2734da
ram <ramjawade.dev@gmail.com>
2026-05-17 15:26:05 +0530

    error fixe

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
 
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import uvicorn
 from fastapi import FastAPI
 from app.database import engine
